f88ad593a2369423b14bddaa0b7b5aba93d56ba9
eminberkayd <eminberkaydaglar@gmail.com>
2023-11-15 20:20:16 +0300

    creating resource follow

--- backend/src/follow/follow.module.ts
+++ backend/src/follow/follow.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { FollowController } from './follow.controller';
 import { FollowService } from './follow.service';
+import { FollowController } from './follow.controller';
 
 @Module({
   controllers: [FollowController],
-  providers: [FollowService]
+  providers: [FollowService],
 })
 export class FollowModule {}
