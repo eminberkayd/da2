9e5a72921df551e790430cb9c06e9e448e34be46
eminberkayd <eminberkaydaglar@gmail.com>
2023-11-16 23:10:08 +0300

    adding users entity.

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+
+@Entity({ name: "users" })
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 30 })
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    profile_photo: string;
+
+    @Column({ length: 255 })
+    biography: string;
+
+    @Column()
+    timezone: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @Column()
+    updated_at: Date;
+}
