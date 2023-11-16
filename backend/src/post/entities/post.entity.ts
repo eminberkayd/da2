c00b9dd00a3fbdccd5780762448660f87709bf01
eminberkayd <eminberkaydaglar@gmail.com>
2023-11-16 23:10:29 +0300

    adding post entity

--- backend/src/post/entities/post.entity.ts
+++ backend/src/post/entities/post.entity.ts
@@ -1,13 +1,31 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToOne } from "typeorm";
+import { CreatePostDto } from "../dto";
+import { User } from "src/user";
 
 @Entity()
 export class Post {
-    constructor(){
+    constructor({text}: CreatePostDto){
 
     }
-    
+
     @PrimaryGeneratedColumn()
     id: number
 
 
+    @Column({
+        type: "text"
+    })
+    text_content: string;
+
+    @Column()
+    image_url: string;
+
+    @OneToOne(() => User)
+    user_id: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @Column()
+    updated_at: Date;
 }
