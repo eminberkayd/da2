0df913634be4b274e19b53a9bb0c5e61bb99eab9
eminberkayd <eminberkaydaglar@gmail.com>
2023-11-17 22:15:51 +0300

    creating CreateUserDto

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    userName: string;
+    fullName: string | undefined;
+    email: string;
+    password: string;
+    profilePhotoUrl: string | undefined;
+    biography: string | undefined;
+    timezone: string | undefined;
+}
