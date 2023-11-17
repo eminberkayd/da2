f7e6b455ea89688b79ddee37678e8a9e7b9a57fb
eminberkayd <eminberkaydaglar@gmail.com>
2023-11-17 22:14:54 +0300

    adding user entity

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,13 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+import { Entity, Column, BaseEntity, BeforeInsert, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import * as bcrpyt from "bcrypt";
+import { CreateUserDto } from "../dto";
 
-@Entity({ name: "users" })
-export class User {
+@Entity('user')
+export class User extends BaseEntity {
+
+    constructor(createUserDto: CreateUserDto){
+        super();
+        this.username = createUserDto.userName;
+        this.fullname = createUserDto.fullName;
+        this.email = createUserDto.email;
+        this.password = createUserDto.password;
+        this.profile_photo = createUserDto.profilePhotoUrl;
+        this.timezone = createUserDto.timezone || "UTC";
+    }
 
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ length: 30 })
+    @Column({
+        length: 30,
+        nullable: false,
+        unique: true
+    })
     username: string;
+
+    @Column({
+        length: 30,
+        nullable: true
+    })
+    fullname: string | undefined;
 
     @Column()
     email: string;
@@ -15,18 +37,27 @@ export class User {
     @Column()
     password: string;
 
-    @Column()
-    profile_photo: string;
+    @Column({
+        nullable: true
+    })
+    profile_photo: string | undefined;
 
     @Column({ length: 255 })
     biography: string;
 
-    @Column()
+    @Column({
+        default: "UTC"
+    })
     timezone: string;
 
     @CreateDateColumn()
     created_at: Date;
 
-    @Column()
+    @UpdateDateColumn()
     updated_at: Date;
+
+    @BeforeInsert()
+    async hashPassword(){
+        this.password = await bcrpyt.hash(this.password, 10);
+    }
 }
