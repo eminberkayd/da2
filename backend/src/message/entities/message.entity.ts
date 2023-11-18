5de2944fb7688a46701db41ccd797b4ff65fc0c5
eminberkayd <eminberkaydaglar@gmail.com>
2023-11-18 17:38:24 +0300

    creating message entity

--- backend/src/message/entities/message.entity.ts
+++ backend/src/message/entities/message.entity.ts
@@ -1,1 +1,37 @@
-export class Message {}
+import { User } from 'src/user/entities';
+import {
+    BaseEntity,
+    Entity,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    JoinColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    Column,
+} from 'typeorm';
+
+@Entity({ name: 'messages' })
+export class Message extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'sender_id' })
+    sender: User;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'receiver_id' })
+    receiver: User;
+
+    @Column({
+        type: 'text',
+        nullable: false
+    })
+    message_content: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
